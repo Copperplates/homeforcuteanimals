7b4fbc41a8cf5cd3e4b77484e3a541ab368c1ead
Froggit Cheng <142158229+Copperplates@users.noreply.github.com>
2024-04-09 16:58:53 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    onLaunch: function() {
+      console.log('App Launch');
+      //获取openid、UnionID、session_key
+      uni.login({
+        provider: 'weixin',
+        success: function(loginRes) {
+          if (loginRes.code) {
+            // 在这里请求后端接口
+          }
+        }
+      });
+    },
+    onShow: function() {
+      console.log('App Show');
+    },
+    onHide: function() {
+      console.log('App Hide');
+    },
+  };
+</script>
+
+<style>
+  /*每个页面公共css */
+</style>
